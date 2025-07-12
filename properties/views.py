a086ce3e2f640f788d41d5f56b4222b43b5f7fb6
shymaaismail <shymaa.m.ismail@gmail.com>
2025-07-12 15:35:46 +0300

    Refactor property_list view to return properties in a structured JSON format

--- properties/views.py
+++ properties/views.py
@@ -1,7 +1,10 @@
 from django.http import JsonResponse
 from .utils import get_all_properties
+from django.views.decorators.cache import cache_page
 
 @cache_page(60 * 15)
 def property_list(request):
     properties = get_all_properties()
-    return JsonResponse(properties, safe=False)
+    return JsonResponse({
+        "data": properties
+    })
